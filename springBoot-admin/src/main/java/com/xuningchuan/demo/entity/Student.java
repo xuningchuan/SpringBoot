27d5b288bf92185f99287bec6f4df1d86e3930bd
xu_nc <xu_nc@suixingpay.com>
2019-07-21 18:37:07 +0800

    test01 第一次提交

--- springBoot-admin/src/main/java/com/xuningchuan/demo/entity/Student.java
+++ springBoot-admin/src/main/java/com/xuningchuan/demo/entity/Student.java
@@ -21,6 +21,7 @@ public class Student {
 
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("1");
+        System.out.println("2");
+        System.out.println("2");
     }
 }
