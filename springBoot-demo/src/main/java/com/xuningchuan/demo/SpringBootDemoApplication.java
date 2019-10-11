e5b0d2865932216fa2a5949b1893e32382f1217d
xu_nc <xu_nc@suixingpay.com>
2019-10-11 20:23:28 +0800

    springBoot新增 spring-demo 模块 用于练习基础demo

---
+++ springBoot-demo/src/main/java/com/xuningchuan/demo/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.xuningchuan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuningchuan
+ * @Title: SpringBootDemoApplication
+ * @Description:
+ * @date 2019/7/8下午8:44
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
---
+++ springBoot-demo/src/main/java/com/xuningchuan/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.xuningchuan.demo.controller;
+
+import com.xuningchuan.demo.entity.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xuningchuan
+ * @Title: DemoController
+ * @Description:
+ * @date 2019/7/8下午8:53
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping("/stu")
+    public Student getStudent() {
+        Student a = new Student();
+        a.setId("1").setName("李明");
+        return a;
+    }
+}
